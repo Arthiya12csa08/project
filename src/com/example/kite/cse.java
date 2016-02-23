215454c520d26333a3ae10306d457b4520211e15
Hariesh94 <joseanisha.jesu@gmail.com>
2016-02-23 15:21:37 -0800

    third commit

--- src/com/example/kite/cse.java
+++ src/com/example/kite/cse.java
@@ -7,12 +7,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class cse extends Activity {
-	TextView com,eng,date,idea;
+	TextView com,eng,date,idea,view;
 	EditText da,id;
 	Button post;
 	@Override
 	  public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.cse);
+	    date=(TextView)findViewById(R.id.date);
+	    com=(TextView)findViewById(R.id.co);
+	    idea=(TextView)findViewById(R.id.idea);
+	    view=(TextView)findViewById(R.id.vp);
+	    eng=(TextView)findViewById(R.id.textView2);
+	    id=(EditText)findViewById(R.id.ml);
+	    da=(EditText)findViewById(R.id.editText1);
+	    post=(Button)findViewById(R.id.button1);
 	}
 }
