33e6f8de79c01bef467756e1c2ef0edd9ee6a07e
Arthiya12csa08 <arthi_nl@yahoo.com>
2016-03-04 02:01:32 -0800

    fourth commit

---
+++ src/com/example/kite/IT.java
@@ -0,0 +1,26 @@
+package com.example.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class cse extends Activity {
+	TextView com,eng,date,idea,view;
+	EditText da,id;
+	Button post;
+	@Override
+	  public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.cse);
+	    date=(TextView)findViewById(R.id.date);
+	    com=(TextView)findViewById(R.id.co);
+	    idea=(TextView)findViewById(R.id.idea);
+	    view=(TextView)findViewById(R.id.vp);
+	    eng=(TextView)findViewById(R.id.textView2);
+	    id=(EditText)findViewById(R.id.ml);
+	    da=(EditText)findViewById(R.id.editText1);
+	    post=(Button)findViewById(R.id.button1);
+	}
+}
